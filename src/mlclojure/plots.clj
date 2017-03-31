59bff6bc8a69a7585d1faae42e3d4bafd99f5ec8
Pavel Tumilovich <p.tumilovich@fasten.com>
2017-03-31 11:19:55 +0300

    A generic draw plot function. Next - visualization

--- src/mlclojure/plots.clj
+++ src/mlclojure/plots.clj
@@ -1,8 +1,20 @@
 (ns mlclojure.plots
   (:import (org.jzy3d.chart2d Chart2d)
-           (org.jzy3d.plot2d.primitives Serie2d Serie2d$Type)))
+           (org.jzy3d.plot2d.primitives Serie2d Serie2d$Type)
+           (org.jzy3d.colors Color)
+           (org.jzy3d.plot3d.primitives ConcurrentLineStrip)))
 
-(let [chart (Chart2d.)]
-  (doto chart
-    (.asTimeChart 5.5 0 100.0 "Iterations" "Cost")
-    (doto (.getSerie "Gradient" Serie2d$Type/LINE))))
+(defn draw-plot [& {:keys [max-x max-y min-y label-x label-y label-serie color]
+                    :or {max-x 10.
+                         max-y 100.
+                         min-y 0.
+                         label-x "X"
+                         label-y "Y"
+                         label-serie "Line"
+                         color Color/RED}}]
+  (let [chart (Chart2d.)]
+   (doto chart
+     (.asTimeChart max-x min-y max-y label-x label-y)
+     (-> (.getSerie label-serie Serie2d$Type/LINE)
+         (.setColor color)))))
+
